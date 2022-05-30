ae9d19c63750aa1c67be10011932a3f4b45fab6e
hanyseyedy <hanyseyedy@gmail.com>
2022-05-31 00:22:19 +0430

    display article list and filter

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
+from statistics import mode
 from django.contrib import admin
 from .models import Articles
 
 # Register your models here.
-admin.site.register(Articles)
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'publish', 'status')
+    list_filter = ('publish', 'status')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+    ordering = ['-publish']
+
+
+admin.site.register(Articles, ArticleAdmin)
