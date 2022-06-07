1efeab36a20d087fa95d8d8375ce8d25da134749
hanyseyedy <hanyseyedy@gmail.com>
2022-06-07 22:43:12 +0430

    gregorian to jalali and persian numbers (17, 18)

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,7 @@ from statistics import mode
 import django
 from django.db import models
 from django.utils import timezone
+from extentions.utils import jalali_converter
 
 # Create your models here.
 
@@ -29,3 +30,8 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def jpublish(self):
+        return jalali_converter(self.publish)
+
+    jpublish.short_description = "زمان انتشار"
