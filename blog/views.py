a8ad2189394e7c77d431ad12ae787cc61e101d0a
hanyseyedy <hanyseyedy@gmail.com>
2022-06-11 22:31:20 +0430

    navbar- rtl farsi font (20 - 21)

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
-from .models import Article
+from .models import Article, Category
 
 # Create your views here.
 
@@ -8,7 +8,8 @@ from .models import Article
 def home(request):
     # return HttpResponse("hello world!")
     context = {
-        "articles": Article.objects.filter(status='p').order_by('-publish')
+        "articles": Article.objects.filter(status='p'),
+        "category": Category.objects.filter(status=True)
     }
     return render(request, "blog/home.html", context)
 
