fb08a9306771c996d325bd92c8d52ba943d067f0
hanyseyedy <hanyseyedy@gmail.com>
2022-06-16 23:55:00 +0430

    25 in progress- pagination

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from .models import Article, Category
@@ -6,9 +7,12 @@ from .models import Article, Category
 
 
 def home(request):
-    # return HttpResponse("hello world!")
+    article_list = Article.objects.published()
+    paginator = Paginator(article_list, 3)
+    page = request.GET.get('page')
+    articles = paginator.get_page(page)
     context = {
-        "articles": Article.objects.publised(),
+        "articles": articles,
     }
     return render(request, "blog/home.html", context)
 
